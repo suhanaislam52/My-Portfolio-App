e0ffd7da8204d236abe42f82ac56f3e25dea0810
Suhana Islam <suhanaislam52@gmail.com>
2025-05-13 23:43:01 +0600

    Widget Sizeboxes Update

--- lib/main.dart
+++ lib/main.dart
@@ -18,16 +18,62 @@ class Home extends StatelessWidget {
               Row(
                 children:<Widget>[
                   CircleAvatar(radius:60,),
+                  SizedBox(width:50,),
                   Column(
+                    crossAxisAlignment:CrossAxisAlignment.start,
                     children:<Widget>[
-                      Text("Name"),
-                      Text("Designation")
-                    ]
-                  )
-                ]
-              )
-            ]
-
+                      Text("Suhana Islam",style:TextStyle(fontSize: 30)),
+                      Text("Designation",style:TextStyle(fontSize: 15))
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height:50,),
+              Padding(
+                padding: const EdgeInsets.only(left:30.0),
+                child: Column(
+                  children:<Widget>[
+                    Row(
+                      children:<Widget>[
+                        Icon(Icons.school,size:40),
+                        SizedBox(width:20),
+                        Text("School name",style:TextStyle(fontSize: 20))
+                      ],
+                    ),
+                    Row(
+                      children:<Widget>[
+                        Icon(Icons.business_center_sharp,size:40),
+                        SizedBox(width:20),
+                        Text("School name",style:TextStyle(fontSize: 20))
+                      ],
+                    ),
+                    Row(
+                      children:<Widget>[
+                        Icon(Icons.location_pin,size:40),
+                        SizedBox(width:20),
+                        Text("School name",style:TextStyle(fontSize: 20))
+                      ],
+                    ),
+                    Row(
+                      children:<Widget>[
+                        Icon(Icons.email,size:40),
+                        SizedBox(width:20),
+                        Text("School name",style:TextStyle(fontSize: 20))
+                      ],
+                    ),
+                    Row(
+                      children:<Widget>[
+                        Icon(Icons.phone,size:40),
+                        SizedBox(width:20),
+                        Text("School name",style:TextStyle(fontSize: 20))
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Text("About Me"),
+              Text("Created By SUHANA ISLAM")
+            ],
         ),
       ),
     );
