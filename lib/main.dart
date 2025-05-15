01368a79276695475b25741c36071edab0bec859
Suhana Islam <suhanaislam52@gmail.com>
2025-05-15 23:42:59 +0600

    Profile Details Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home(),debugShowCheckedModeBanner: false,));
+  runApp(MaterialApp(home: Home(), debugShowCheckedModeBanner: false));
 }
 
 class Home extends StatelessWidget {
@@ -16,13 +16,17 @@ class Home extends StatelessWidget {
           children: <Widget>[
             Row(
               children: <Widget>[
-                CircleAvatar(radius: 60,backgroundImage:AssetImage("images/profile.PNG")),
+                CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage("images/profile.PNG")),
                 SizedBox(width: 50),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Suhana Islam", style: TextStyle(fontSize: 30,fontFamily:"Roboto")),
-                    Text("Designation", style: TextStyle(fontSize: 15)),
+                    Text("Suhana Islam",
+                        style:
+                        TextStyle(fontSize: 30, fontFamily: "Roboto")),
+                    Text("Undergraduate Student", style: TextStyle(fontSize: 15)),
                   ],
                 ),
               ],
@@ -36,52 +40,111 @@ class Home extends StatelessWidget {
                     children: <Widget>[
                       Icon(Icons.school, size: 40),
                       SizedBox(width: 25),
-                      Text("School name", style: TextStyle(fontSize: 20)),
+                      Text("School : SCHOLASTICA",
+                          style: TextStyle(fontSize: 20)),
                     ],
                   ),
-                  SizedBox(height:10,),
+                  SizedBox(height: 10),
                   Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Icon(Icons.business_center_sharp, size: 40),
                       SizedBox(width: 25),
-                      Text("School name", style: TextStyle(fontSize: 20)),
+                      Flexible(
+                        child: Text(
+                          "University: North South University",
+                          style: TextStyle(fontSize: 20),
+                          softWrap: true,
+                        ),
+                      ),
                     ],
                   ),
-                  SizedBox(height:10,),
+                  SizedBox(height: 10),
                   Row(
                     children: <Widget>[
                       Icon(Icons.location_pin, size: 40),
                       SizedBox(width: 25),
-                      Text("School name", style: TextStyle(fontSize: 20)),
+                      Text("Dhaka , Bangladesh",
+                          style: TextStyle(fontSize: 20)),
                     ],
                   ),
-                  SizedBox(height:10,),
+                  SizedBox(height: 10),
                   Row(
                     children: <Widget>[
                       Icon(Icons.email, size: 40),
                       SizedBox(width: 25),
-                      Text("School name", style: TextStyle(fontSize: 20)),
+                      Text("suhanaislam52@gmail.com",
+                          style: TextStyle(fontSize: 20)),
                     ],
                   ),
-                  SizedBox(height:10,),
+                  SizedBox(height: 10),
                   Row(
                     children: <Widget>[
-                      Icon(Icons.phone, size: 40),
+                      Icon(Icons.work, size: 40),
                       SizedBox(width: 25),
-                      Text("School name", style: TextStyle(fontSize: 20)),
+                      Text("Interest Field: Software", style: TextStyle(fontSize: 20)),
                     ],
                   ),
-                  SizedBox(height:10,),
+                  SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(Icons.link, size: 40),
+                      SizedBox(width: 25),
+                      Expanded(
+                        child: Text(
+                          "LinkedIn: https://www.linkedin.com/in/suhana-islam-a87803364/",
+                          style: TextStyle(fontSize: 16),
+                          softWrap: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(Icons.code, size: 40),
+                      SizedBox(width: 25),
+                      Expanded(
+                        child: Text(
+                          "GitHub: https://github.com/suhanaislam52",
+                          style: TextStyle(fontSize: 16),
+                          softWrap: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
                 ],
               ),
             ),
-            SizedBox(height:50,),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text("About Me: I am currently an undergraduate student majoring in computer science.",style:TextStyle(fontSize: 22)),
+            SizedBox(height: 15),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    "About Me:",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 10.0),
+                  child: Text(
+                    "Undergraduate Computer Science student interested in software, web development, and related fields.",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height:20,),
-            Text("Created By SUHANA ISLAM"),
+
+            SizedBox(height: 20),
+            Text(
+              "Created By SUHANA ISLAM",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+
           ],
         ),
       ),
